e5fe7052281807f131443007f98d0a1372c9bfd0
Ossama Othman <ossama.othman@intel.com>
2014-01-16 14:47:37 -0800

    [TIVI-2213] Added weston start-up files, and split config to subpackage.
    Change-Id: I4fa9759d4343655ec56a717a90917629c8fc32de
    Signed-off-by: Ossama Othman <ossama.othman@intel.com>

---
+++ weston.sh
@@ -0,0 +1,8 @@
+export ELM_ENGINE=wayland_egl
+export ECORE_EVAS_ENGINE=wayland_egl
+
+# Make EFL apps use the wayland-based input method.
+export ECORE_IMF_MODULE=wayland
+
+# also export dbus session address for dbus clients (details on bug TIVI-1686 [https://bugs.tizen.org/jira/browse/TIVI-1686])
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$UID/dbus/user_bus_socket
